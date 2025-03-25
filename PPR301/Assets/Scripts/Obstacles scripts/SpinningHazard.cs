8ceb4fd184884ad6a87c7afb12826bc6ea95a1f4
AliAK03 <ali.alkathmi03@gmail.com>
2025-03-25 13:46:39 +1100

    Added some obstacle Prefabs in Assets.
    Use these to create some level ideas.

--- PPR301/Assets/Scripts/Obstacles scripts/SpinningHazard.cs
+++ PPR301/Assets/Scripts/Obstacles scripts/SpinningHazard.cs
@@ -6,13 +6,27 @@ public class SpinningHazard : MonoBehaviour
 {
     public int hazards;
     public float distance;
-    public GameObject hazard;
+    private float distance2;
+    public GameObject obj;
+    public Transform parentObject; // Assign an empty GameObject in the Inspector
     // Start is called before the first frame update
     void Start()
     {
+        distance2 = distance;
         for(int i = 0; i < hazards; i++)
         {
             Debug.Log("hello");
+            //x
+            GameObject obj1 = Instantiate(obj, new Vector3(transform.position.x + distance2,transform.position.y,transform.position.z), Quaternion.identity);
+            GameObject obj2 = Instantiate(obj, new Vector3(transform.position.x - distance2,transform.position.y,transform.position.z), Quaternion.identity);
+            //z
+            GameObject obj3 = Instantiate(obj, new Vector3(transform.position.x, transform.position.y, transform.position.z + distance2), Quaternion.identity);
+            GameObject obj4 = Instantiate(obj, new Vector3(transform.position.x, transform.position.y, transform.position.z - distance2), Quaternion.identity);
+            distance2 = distance2 + distance;
+            obj1.transform.SetParent(parentObject); // Set parent
+            obj2.transform.SetParent(parentObject); // Set parent
+            obj3.transform.SetParent(parentObject); // Set parent
+            obj4.transform.SetParent(parentObject); // Set parent
         }
     }
 
