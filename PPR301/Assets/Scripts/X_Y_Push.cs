7ccae5a983e25837faf7fd5b5fab422b0eb1c6df
AliAK03 <ali.alkathmi03@gmail.com>
2025-03-26 21:41:57 +1100

    making a new level idea.

---
+++ PPR301/Assets/Scripts/X_Y_Push.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class X_Y_Push : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > -91)
+        {
+            transform.position = new Vector3(-91,transform.position.y,transform.position.z);
+        }
+    }
+}
