c2ec6c6f4838859e42792b2f819eb3371330ecc3
AliAK03 <ali.alkathmi03@gmail.com>
2025-04-04 18:41:28 +1100

    changes to demo scene

--- PPR301/Assets/Scripts/Player/PlayerMovement.cs
+++ PPR301/Assets/Scripts/Player/PlayerMovement.cs
@@ -61,7 +61,7 @@ public class PlayerMovement : MonoBehaviour
 
         readyToJump = true;
         originalScale = crouchScaleObject.localScale;
-        playerCamera = Camera.main;
+        //playerCamera = Camera.main;
         anim = GetComponentInChildren<Animator>();
 
         moveBehaviour = DefaultMovement;
