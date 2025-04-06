8371b27dfaa49fdbcc5443f5f25c79e80bb5682b
AliAK03 <ali.alkathmi03@gmail.com>
2025-04-06 19:26:40 +1000

    fixed spawn issues

--- PPR301/Assets/Scripts/Player/OutOfBounds.cs
+++ PPR301/Assets/Scripts/Player/OutOfBounds.cs
@@ -6,15 +6,31 @@ public class OutOfBounds : MonoBehaviour
 {
     public Vector3 respawnLocation;
     public Vector3 respawnLocation2;
+    //public Vector3 originalLocation;
 
     public Vector3[] spawnLocationsArray;
     public Vector3 currentRespawnLocation;
 
     void Start()
     {
+        //originalLocation = transform.position;
+        //currentRespawnLocation = originalLocation;
+        currentRespawnLocation =  transform.position;
+        //assign array of spawning locations
         spawnLocationsArray = new Vector3[2];
         spawnLocationsArray[0] = respawnLocation;
         spawnLocationsArray[1] = respawnLocation2;
+    }
+    void Update()
+    {
+        fallOffMap();
+    }
+    void fallOffMap()
+    {
+        if(transform.position.y < -8)
+        {
+            transform.position = currentRespawnLocation;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
