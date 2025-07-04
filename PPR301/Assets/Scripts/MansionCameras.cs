8faacddb922b29eafab401d189584e5ab4753638
AliAK03 <ali.alkathmi03@gmail.com>
2025-07-04 17:10:15 +1000

    made reward appear in riddle puzzle

--- PPR301/Assets/Scripts/MansionCameras.cs
+++ PPR301/Assets/Scripts/MansionCameras.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class MansionCameras : MonoBehaviour
 {
+    public Camera camera1;  // Reference to Camera 1
+    public Camera camera2;  // Reference to Camera 2
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (camera1) camera1.depth = 1;
+        if (camera2) camera2.depth = 0;
     }
 
     // Update is called once per frame
