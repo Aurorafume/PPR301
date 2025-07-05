95763e0e87b78e82fecbe53d24e5ff481cbfd2ad
AliAK03 <ali.alkathmi03@gmail.com>
2025-07-05 17:45:36 +1000

    Made it so you can speak to each raccoon.

--- PPR301/Assets/Assets/Raccoon Character/DialogueBox.cs
+++ PPR301/Assets/Assets/Raccoon Character/DialogueBox.cs
@@ -14,11 +14,15 @@ public class DialogueBox : MonoBehaviour
     public List<string> sentenceList = new List<string>();
     public List<string> sentenceList2 = new List<string>();
     public int sentenceIndex;
+    public static bool isTalking;
     //distance
-    public float dist;
+    public float dist; //individual distance
+    public static float distFromRaccoonList; //distance from anything in the raccoonList
     public Transform player;
     //icon
     public GameObject icon;
+    //raccoon list
+    public static List<DialogueBox> raccoonList = new List<DialogueBox>(); 
 
     // Start is called before the first frame update
     void Start()
@@ -26,14 +30,17 @@ public class DialogueBox : MonoBehaviour
         anim = GetComponent<Animator>();
         sentenceListList.Add(sentenceList);
         sentenceListList.Add(sentenceList2);
-        //anim.SetBool("talking", false);
+
+        raccoonList.Add(this);
     }
 
     // Update is called once per frame
     void Update()
     {
         Billboard();
-        Talk();
+        //Talk();
+        Talk2();
+        Debug.Log(isTalking);
     }
     void Billboard()
     {
@@ -94,6 +101,69 @@ public class DialogueBox : MonoBehaviour
             //reset speech
             sentenceIndex = 0;
             speechBubble.SetActive(false);
+        }
+    }
+    void Talk2()
+    {
+        bool nearAnyRaccoon = false;
+        dist = Vector3.Distance(player.position, transform.position);
+        foreach(DialogueBox rac in raccoonList)
+        {
+            distFromRaccoonList = Vector3.Distance(player.position, rac.transform.position);
+            if(distFromRaccoonList < 3)
+            {
+                nearAnyRaccoon = true;
+                //talk
+                if(Input.GetMouseButtonDown(0))
+                {
+                    isTalking = true;
+                }
+                break;
+            }
+        }
+        if (!nearAnyRaccoon)
+        {
+            icon.SetActive(false);
+            //reset talking
+            isTalking = false;
+            anim.SetBool("talking", false);
+            //reset speech
+            sentenceIndex = 0;
+            speechBubble.SetActive(false);
+        }
+        else
+        {
+            //icon
+            icon.SetActive(true);
+            icon.transform.LookAt(Camera.main.transform.position, Vector3.up);
+            icon.transform.Rotate(0f, 180f, 0f);
+            //talk
+            if(dist < 3)
+            if(Input.GetMouseButtonDown(0))
+            {
+                anim.SetBool("talking", true);
+                speechBubble.SetActive(true);
+                if(sentenceIndex < sentenceListList[listIndex].Count)
+                {
+                    sentenceIndex = sentenceIndex + 1;
+                    textSign.text = sentenceListList[listIndex][sentenceIndex - 1];
+                }
+                else if(listIndex < sentenceListList.Count - 1)
+                {
+                    listIndex = listIndex + 1;
+                    sentenceIndex = 0;
+                    speechBubble.SetActive(false);
+                    anim.SetBool("talking", false);
+                }
+                else
+                {
+                    Debug.Log("Reached end of dialogue");
+                    sentenceIndex = 0;
+                    speechBubble.SetActive(false);
+                    anim.SetBool("talking", false);
+                }
+            }
+            
         }
     }
 }
