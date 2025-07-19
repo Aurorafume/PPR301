78fb110394fa356c9b0c34066b1a7b0f8855045a
AliAK03 <ali.alkathmi03@gmail.com>
2025-07-19 18:55:57 +1000

    made paws appear and fade out in menu.
    click to make paws appear in the menu or pause menu.

--- PPR301/Assets/Assets/CursorController.cs
+++ PPR301/Assets/Assets/CursorController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorController : MonoBehaviour
 {
     public Texture2D cursorTextureDefault;
     public Vector2 clickPosition = Vector2.zero;
-    // Start is called before the first frame update
+    //cursor prefab
+    public GameObject cursorClickImagePrefab; // Prefab with UI Image
+    public Canvas canvas; // Reference to the UI canvas
+     public float fadeDuration = 1f;
     void Start()
     {
         Vector2 centerHotspot = new Vector2(cursorTextureDefault.width / 2, cursorTextureDefault.height / 2);
@@ -16,6 +20,37 @@ public class CursorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 spawnPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvas.transform as RectTransform,
+                Input.mousePosition,
+                canvas.worldCamera,
+                out spawnPos);
+
+            GameObject clickImage = Instantiate(cursorClickImagePrefab, canvas.transform);
+            RectTransform rect = clickImage.GetComponent<RectTransform>();
+            rect.anchoredPosition = spawnPos;
+
+            Image img = clickImage.GetComponent<Image>();
+            StartCoroutine(FadeAndDestroy(img, fadeDuration));
+        }
+    }
+    System.Collections.IEnumerator FadeAndDestroy(Image image, float duration)
+    {
+        float elapsed = 0f;
+        Color originalColor = image.color;
+
+        while (elapsed < duration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsed / duration);
+            image.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        image.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
+        Destroy(image.gameObject);
     }
 }
